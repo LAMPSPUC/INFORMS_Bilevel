59b866e610bb8e148c48fe874148a6ae5bed3d70
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2020-10-11 16:31:18 -0300

    update

--- script.jl
+++ script.jl
@@ -32,27 +32,16 @@ optimize!(model)
 
 model = BilevelModel(Ipopt.Optimizer, mode = BilevelJuMP.ProductMode(1e-5))
 
-@variable(Upper(model), qS, start = 15)
+@variable(Upper(model), 0 <= qS <= 100, start = 15)
 
-@constraint(Upper(model), qS >= 0)
-@constraint(Upper(model), qS <= 100)
-
-@variable(Lower(model), gS, start = 20)
-@variable(Lower(model), gR1, start = 40)
-@variable(Lower(model), gR2, start = 40)
-@variable(Lower(model), gD, start = 0)
+@variable(Lower(model), 0 <= gS, start = 20)
+@variable(Lower(model), 0 <= gR1 <= 40, start = 40)
+@variable(Lower(model), 0 <= gR2 <= 40, start = 40)
+@variable(Lower(model), 0 <= gD <= 100, start = 0)
 
 @objective(Lower(model), Min, 50gR1 + 100gR2 + 1000gD)
 @constraint(Lower(model), b, gS + gR1 + gR2 + gD == 100)
 @constraint(Lower(model), gS <= qS)
-@constraint(Lower(model), gR1 <= 40)
-@constraint(Lower(model), gR2 <= 40)
-@constraint(Lower(model), gD <= 100)
-
-@constraint(Lower(model), gS >= 0)
-@constraint(Lower(model), gR1 >= 0)
-@constraint(Lower(model), gR2 >= 0)
-@constraint(Lower(model), gD >= 0)
 
 @variable(Upper(model), lambda, DualOf(b), start = 1_000)
 @objective(Upper(model), Max, lambda*gS)
@@ -78,35 +67,24 @@ const CBC_BRIDGED = MOI.Bridges.full_bridge_optimizer(CBC_CACHED, Float64)
 opt = QuadraticToBinary.Optimizer{Float64}(CBC_BRIDGED)
 model = BilevelModel(() -> opt, mode = BilevelJuMP.SOS1Mode())
 
-@variable(Upper(model), qS)
+@variable(Upper(model), 0 <= qS <= 100)
 
-@constraint(Upper(model), qS >= 0)
-@constraint(Upper(model), qS <= 100)
-
-@variable(Lower(model), gS)
-@variable(Lower(model), gR1)
-@variable(Lower(model), gR2)
-@variable(Lower(model), gD)
+@variable(Lower(model), 0 <= gS <= 1000)
+@variable(Lower(model), 0 <= gR1 <= 40)
+@variable(Lower(model), 0 <= gR2 <= 40)
+@variable(Lower(model), 0 <= gD <= 100)
 
 @objective(Lower(model), Min, 50gR1 + 100gR2 + 1000gD)
 @constraint(Lower(model), b, gS + gR1 + gR2 + gD == 100)
 @constraint(Lower(model), gS <= qS)
-@constraint(Lower(model), gR1 <= 40)
-@constraint(Lower(model), gR2 <= 40)
-@constraint(Lower(model), gD <= 100)
 
-@constraint(Lower(model), gS >= 0)
-@constraint(Lower(model), gR1 >= 0)
-@constraint(Lower(model), gR2 >= 0)
-@constraint(Lower(model), gD >= 0)
-
-@variable(Upper(model), lambda, DualOf(b))
+@variable(Upper(model), -10_000 <= lambda <= 10_000, DualOf(b))
 @objective(Upper(model), Max, lambda*gS)
 
 optimize!(model)
 
-# @test objective_value(model) ≈ 20_000  atol=1e-1
-# @test BilevelJuMP.lower_objective_value(model) ≈ 6_000  atol=1e-1
-# @test value(qS) ≈ 20 atol=1e-3
-# @test value.(gS) ≈ 20 atol=1e-3
-# @test value(lambda) ≈ 1_000 atol=1e-3
+@test objective_value(model) ≈ 20_000  atol=20
+@test BilevelJuMP.lower_objective_value(model) ≈ 6_000  atol=1e-1
+@test value(qS) ≈ 20 atol=1e-3
+@test value.(gS) ≈ 20 atol=1e-3
+@test value(lambda) ≈ 1_000 atol=1e-3
