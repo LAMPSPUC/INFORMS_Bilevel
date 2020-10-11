b98b83cb19562b470e0faf15bc35b0fe8347d5d7
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2020-10-11 16:14:53 -0300

    update PS mip

--- script.jl
+++ script.jl
@@ -2,6 +2,7 @@ using BilevelJuMP
 using Cbc
 using Ipopt
 using JuMP
+using QuadraticToBinary
 using Test
 
 # Example slide 9
@@ -27,9 +28,7 @@ optimize!(model)
 @test value(x) ≈ 2
 @test value(y) ≈ 0
 
-
-
-# Power systems example
+# Power systems example NLP
 
 model = BilevelModel(Ipopt.Optimizer, mode = BilevelJuMP.ProductMode(1e-5))
 
@@ -65,3 +64,49 @@ optimize!(model)
 @test value(qS) ≈ 20 atol=1e-3
 @test value.(gS) ≈ 20 atol=1e-3
 @test value(lambda) ≈ 1_000 atol=1e-3
+
+
+# Power systems example MIP
+
+const CBC_OPTIMIZER = Cbc.Optimizer()
+MOI.set(CBC_OPTIMIZER, MOI.Silent(), true)
+const CBC_CACHE = MOIU.UniversalFallback(MOIU.Model{Float64}())
+const CBC_CACHED = MOIU.CachingOptimizer(CBC_CACHE, CBC_OPTIMIZER)
+const CBC_BRIDGED = MOI.Bridges.full_bridge_optimizer(CBC_CACHED, Float64)
+            MOI.Bridges.add_bridge(CBC_BRIDGED, MOI.Bridges.Constraint.SOCtoNonConvexQuadBridge{Float64})
+
+opt = QuadraticToBinary.Optimizer{Float64}(CBC_BRIDGED)
+model = BilevelModel(() -> opt, mode = BilevelJuMP.SOS1Mode())
+
+@variable(Upper(model), qS)
+
+@constraint(Upper(model), qS >= 0)
+@constraint(Upper(model), qS <= 100)
+
+@variable(Lower(model), gS)
+@variable(Lower(model), gR1)
+@variable(Lower(model), gR2)
+@variable(Lower(model), gD)
+
+@objective(Lower(model), Min, 50gR1 + 100gR2 + 1000gD)
+@constraint(Lower(model), b, gS + gR1 + gR2 + gD == 100)
+@constraint(Lower(model), gS <= qS)
+@constraint(Lower(model), gR1 <= 40)
+@constraint(Lower(model), gR2 <= 40)
+@constraint(Lower(model), gD <= 100)
+
+@constraint(Lower(model), gS >= 0)
+@constraint(Lower(model), gR1 >= 0)
+@constraint(Lower(model), gR2 >= 0)
+@constraint(Lower(model), gD >= 0)
+
+@variable(Upper(model), lambda, DualOf(b))
+@objective(Upper(model), Max, lambda*gS)
+
+optimize!(model)
+
+# @test objective_value(model) ≈ 20_000  atol=1e-1
+# @test BilevelJuMP.lower_objective_value(model) ≈ 6_000  atol=1e-1
+# @test value(qS) ≈ 20 atol=1e-3
+# @test value.(gS) ≈ 20 atol=1e-3
+# @test value(lambda) ≈ 1_000 atol=1e-3
